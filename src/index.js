21c3e759574148f030196e0229c4796c8a30464e
Adam Jones <adam@enmaso.com>
2017-07-31 18:38:12 -0700

    Initial Routes
    Initial Routes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,33 @@ require('dotenv').config()
 
 import express from 'express'
 import bodyParser from 'body-parser'
+import logger from './lib/logger'
+import routes from './routes'
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+// Service middleware
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(require('morgan')('short', {stream: logger.stream}))
+
+// Service routes
+app.use('/auth', routes)
+
+// Service status route
+app.get('/status', (req, res) => {
+  res.status(200).send('OK')
+})
+
+// If Route not found, 404
+app.all('*', (req, res) => {
+  res.status(404).send('Not Found')
+})
+
+// Run service
+app.listen(PORT, () => {
+  logger.debug(`[${process.env.NODE_ENV}] Auth-Service started on port ${PORT}`)
+})
+
+export default app
