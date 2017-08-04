5bcd7611b38f9485dea347642ef92911c8d89dcf
Adam Jones <adam@enmaso.com>
2017-08-04 16:09:18 -0700

    Modified create action
    Modified create action

--- src/routes/create.js
+++ src/routes/create.js
@@ -1,36 +1,65 @@
 import { Router } from 'express'
-import _ from 'lodash'
 import logger from '../lib/logger'
 import Account from '../lib/account'
+import passport from 'passport'
+import { Strategy } from 'passport-local'
+import validator from 'validator'
+
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+})
+passport.deserializeUser(function (obj, done) {
+  done(null, obj)
+})
+passport.use(new Strategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, (req, email, password, done) => {
+  // Validate email
+  if(!validator.isEmail(email)) {
+    return done(null, false)
+  }
+  // Validate password
+  if(!validator.isLength(password, 4)) {
+    return done(null, false)
+  }
+  // Find or Create Account record
+  Account.findOne({
+    email: email
+  }, (err, account) => {
+    if(err) {
+      // Server error
+      return done(err, false)
+    } else if(account) {
+      // Account already exists, check password
+      if(!Account.match(password, account.password)) {
+        return done(null, false)
+      } else {
+        return done(null, account)
+      }
+    } else {
+      // Account does not exist, create one
+      let account = new Account()
+      account.email = email
+      account.password = password
+      account.save(err => {
+        if(err) {
+          logger.error(err.stack)
+          return done(err, false)
+        } else {
+          return done(null, account)
+        }
+      })
+    }
+  })
+}))
 
 const router = Router()
 
-// POST /auth/create
-router.post('/', (req, res) => {
-  let params = _.pick(req.body, 'email', 'password')
-  if(!params.email || !params.password) {
-    logger.error('Missing Parameters')
-    res.status(400).send('Missing Parameters')
-  } else {
-    let account = new Account()
-    account.email = params.email
-    account.password = params.password
-    account.save(err => {
-      if(err) {
-        logger.debug(err, err.stack)
-        if(err.code === 11000) {
-          res.status(400).send('Account Exists')
-        } else {
-          res.status(400).send('Error Creating Account')
-        }
-      } else {
-        res.status(201).json({
-          account: account
-        })
-      }
-    })
-  }
+router.post('/', passport.authenticate('local'), (req, res) => {
+  res.send('OK')
 })
-
 
 export default router
