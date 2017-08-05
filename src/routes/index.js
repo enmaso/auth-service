ff264527c96161fbb633b3ce5057b95c2daa4528
Adam Jones <adam@enmaso.com>
2017-08-05 14:23:19 -0700

    Removed account auth
    Removed account auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,4 @@
 import express from 'express'
-import create from './create'
-import validate from './validate'
 import box from './box'
 import dropbox from './dropbox'
 import twitter from './twitter'
@@ -10,12 +8,6 @@ import evernote from './evernote'
 import linkedin from './linkedin'
 
 const router = express.Router()
-
-// Route Create Account
-router.use('/create', create)
-
-// Route Validate Account
-router.use('/validate', validate)
 
 // Route Authorize Box Service
 router.use('/box', box)
